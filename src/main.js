7b7f7ff1266eea3902d442cb1bf00c9cc3414dc1
zubeir <zubwd1@gmail.com>
2024-08-19 12:48:08 +0100

    Fist commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Importation de Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Importation de Bootstrap JS et Popper.js
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
